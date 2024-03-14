f7caa0bc5c9482325e64fbae43e87c62d89bfd0b
Fr0ntcoder <jambox228@gmail.com>
2024-03-14 19:01:34 +0800

    time-block

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TaskModule } from 'src/task/task.module'
+import { TimeBlockModule } from 'src/time-block/time-block.module'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule]
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		TaskModule,
+		TimeBlockModule
+	]
 })
 export class AppModule {}
