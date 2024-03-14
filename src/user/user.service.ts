94c5f369593dbd008aa14d46af72f407d5e6d816
Fr0ntcoder <jambox228@gmail.com>
2024-03-14 14:11:08 +0800

    user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { hash } from 'argon2'
+import { startOfDay, subDays } from 'date-fns'
 import { AuthDto } from 'src/auth/dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
-
+import { UserDto } from 'src/user/dto/user.dto'
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
@@ -29,6 +30,82 @@ export class UserService {
 
 		return this.prisma.user.create({
 			data: user
+		})
+	}
+
+	async getProfile(id: string) {
+		const profile = await this.getById(id)
+
+		const totalTasks = profile.tasks.length
+		const completedTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true
+			}
+		})
+
+		const todayStart = startOfDay(new Date())
+		const weekStart = startOfDay(subDays(new Date(), 7))
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: todayStart.toISOString()
+				}
+			}
+		})
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: weekStart.toISOString()
+				}
+			}
+		})
+
+		const { password, ...rest } = profile
+
+		return {
+			user: rest,
+			statistics: [
+				{
+					label: 'Общие',
+					value: totalTasks
+				},
+				{
+					label: 'Завершенные задачи',
+					value: completedTasks
+				},
+				{
+					label: 'Задачи на сегодня',
+					value: todayTasks
+				},
+				{
+					label: 'Задачи на неделю',
+					value: weekTasks
+				}
+			]
+		}
+	}
+
+	async update(id: string, dto: UserDto) {
+		let data = dto
+
+		if (dto.password) {
+			data = { ...dto, password: await hash(dto.password) }
+		}
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data,
+			select: {
+				name: true,
+				email: true
+			}
 		})
 	}
 }
